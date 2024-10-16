de5544216fad1c4c9bcdbac46f79744cde2cdfbe
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-16 15:51:30 +0200

    add fifth solution, first folder

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -27,7 +27,8 @@ const firstTeacher = teachers.shift();
 console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-
+teachers.push('Vanessa');
+console.log(teachers);
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
